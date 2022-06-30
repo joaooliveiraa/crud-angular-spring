cf41faa6e6115614737f0da43f30d92eb7cccfa5
Joao Oliveira <joaogetknow@gmail.com>
2022-06-30 17:49:14 -0300

    Add select/save

--- front-angular/src/app/courses/course-form/course-form.component.ts
+++ front-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,32 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../courses/services/courses.service';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
-
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar
+  ) {
     this.form = this.formBuilder.group({
-        name: [null],
-        category: [null]
+      name: [null],
+      category: [null],
     });
-
-   }
-
-  ngOnInit(): void {
   }
 
-  onSubmit(){
+  ngOnInit(): void {}
 
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (result) => console.log(result), error => this.onError());
   }
 
-  onCancel(){
+  onCancel() {}
 
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso', '', { duration: 500 });
   }
-
 }
